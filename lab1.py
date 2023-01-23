92b0fd0b932723750acf8846ed042b2ad44d9108
AlisherMansurov <alisher.mansurov101102@gmail.com>
2023-01-23 15:36:46 +0600

    add new file

--- lab1.py
+++ lab1.py
@@ -1,1 +1,9 @@
-print("Hello")
+St = "Mansurov Alisher Bakhtiarovich 3 kurs"
+print(St)
+a = 5
+b = int(input("press number "))
+if a > b:
+    print("b smaller than a")
+else:
+    print("a smaller than b")
+    
