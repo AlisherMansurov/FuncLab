7551fc75f00d0d8f529a0564a6e05344df6fe5ae
AlisherMansurov <alisher.mansurov101102@gmail.com>
2023-01-26 16:15:52 +0600

    add

--- lab1.py
+++ lab1.py
@@ -2,8 +2,8 @@ St = "Mansurov Alisher Bakhtiarovich 3 kurs"
 print(St)
 a = 5
 b = int(input("press number "))
-if a > b:
+if a > b:   #условие если a больше чем b 
     print("b smaller than a")
-else:
+else:       #при не выполнений прошлого условия if выполняется else
     print("a smaller than b")
     
