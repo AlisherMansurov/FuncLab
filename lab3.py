76ee1f6e3dd29795f70be666032491af6d8f4865
AlisherMansurov <alisher.mansurov101102@gmail.com>
2023-01-30 12:47:23 +0600

    added

---
+++ lab3.py
@@ -0,0 +1,17 @@
+'''
+for i in range(0, 5):
+    print("Hi!")
+
+a = []
+for i in range(0, 5):
+    a.append("шу")
+
+for i in range(0, 5):
+    print(a[i])
+
+a = []
+for i in range(0, 5):
+    b = input()
+    a.append(b)
+print(a)
+'''
